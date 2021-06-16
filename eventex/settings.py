4cdb0a9fdf717338939c2df9295e6034150b0f71
Zackmariano <zack.cmariano@gmail.com>
2021-06-16 20:44:09 -0300

    eventexaws

--- eventex/settings.py
+++ eventex/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', '3.17.206.71']
 
 DEFAULT_FROM_EMAIL = 'contato@eventex.com.br'
 
